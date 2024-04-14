050401c57bad9e1cae58c4e5d9287b04d756b886
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-04-14 08:21:36 -0500

    feat: :sparkles: Se crea el ejercicio 1
    Programa que elige un numero mayor con una funcion de control de flujo y con un operador ternario

--- 04-ejercicios/01.js
+++ 04-ejercicios/01.js
@@ -1,11 +1,23 @@
-function cualEsMayor (a,b) {
+
+// Ejemplo con funcion
+function numeroMayor(a, b) {
     if (a>b) {
-        return a;
+        console.log(a," es mayor")
+    } else if (b>a) {
+        console.log(b, "es mayor")
     } else {
-        return b;
+        console.log("ninguno es mayor")
     }
 }
 
-let mayor = cualEsMayor(10,15);
+let mayor = numeroMayor(10, 5);
 
-console.log(mayor)
+
+// Ejemplo con operador ternario
+function otroEjemplo(a,b) {
+    return (a>b) ? a : b;
+}
+
+let otroMayor = otroEjemplo(10, 15);
+
+console.log(otroMayor);
